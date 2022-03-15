63c3aaab7bb4f6bebb1ace4110adbaff659818cf
Rodrigo Vazquez <tomiivazqueznwn12@gmail.com>
2022-03-15 16:42:24 -0300

    css in the signup_form an adde clases html to the fields

--- mypage/users/forms.py
+++ mypage/users/forms.py
@@ -7,10 +7,36 @@ from .models import Profile
 
 
 class SignUpForm(UserCreationForm):
-    username = forms.CharField(label="Nombre de usuario",max_length=100,required=True)
-    first_name = forms.CharField(label="Nombre",max_length=100,required=True)
-    last_name = forms.CharField(label="Apellido",max_length=100, required=False)
-    email = forms.EmailField(label="Correo Electronico",required=True)
+    username = forms.CharField(label="",max_length=100,required=True, widget=forms.TextInput(
+        attrs={
+            'placeholder':'Nombre de usuario',
+            'class' : 'field'
+        }
+    )
+)
+    
+    first_name = forms.CharField(label="",max_length=100,required=True,widget= forms.TextInput(
+        attrs={
+            'placeholder':'Nombre',
+            'class' : 'field'
+        }
+    )
+)
+    
+    last_name = forms.CharField(label="",max_length=100, required=False,widget= forms.TextInput(
+        attrs={
+            'placeholder':'Apellido',
+            'class' : 'field'
+        }
+    )
+)
+    email = forms.EmailField(label="",required=True,widget= forms.TextInput(
+        attrs={
+            'placeholder':'Correo electronico',
+            'class' : 'field'
+            }
+    )
+)
     
 
     class Meta:
@@ -28,17 +54,24 @@ class SignUpForm(UserCreationForm):
             "password2",
         )
 
+        widgets = {
+            'password1': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password from numbers and letters of the Latin alphabet'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password confirmation'})
+            }
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['password1'].label = 'Contraseña'
-        self.fields['password2'].label = 'Repite tu contraseña'
+        self.fields['password1'].label = ''
+        self.fields['password2'].label = ''
         self.fields['password1'].help_text = None
         self.fields['password2'].help_text = None
+        self.fields['password1'].widget = forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Contraseña nueva','class' : 'field'})
+        self.fields['password2'].widget = forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Repite tu contraseña','class' : 'field'})
 
 
 class UserLoginForm(AuthenticationForm):
-    username = forms.CharField(max_length=100, widget=forms.widgets.TextInput(attrs={'placeholder': 'nickname'}))
-    password = forms.CharField(widget=forms.widgets.PasswordInput(attrs={'placeholder': 'password'}))
+    username = forms.CharField(max_length=100, widget=forms.widgets.TextInput(attrs={'placeholder': ' Nombre de usuario'}))
+    password = forms.CharField(widget=forms.widgets.PasswordInput(attrs={'placeholder': 'Contraseña'}))
 
     class Meta:
         model = AuthenticationForm
